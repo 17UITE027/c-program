01c44102ebbe82b49558d9778eeab47cc5dd65c2
Dinesh272 <30414317+Dinesh272@users.noreply.github.com>
2017-07-29 21:10:19 +0530

    string.c

---
+++ string.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+string str1,str2;
+printf("Enter the string to be reverse");
+scanf("%s",str1);
+srt2=strrev(str1);
+printf("The reverse of the string is %s",str2);
+}
