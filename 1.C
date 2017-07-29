eeb3ed76b8640612fbfe4526d6e87de8104ee4b1
Dinesh272 <30414317+Dinesh272@users.noreply.github.com>
2017-07-29 16:41:44 +0530

    updated 1.c
    c program

--- 1.C
+++ 1.C
@@ -1,1 +1,22 @@
 
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a;
+  printf("Enter the number to be check");
+  scanf("%d",&a);
+  if(a<0)
+  {
+    printf("The given number is negative number");
+  }
+  else if(a>0)
+  {
+    printf("The given number is positive number");
+  }
+  else
+  {
+    printf("The given number is Zero");
+  }
+  getch();
+}
