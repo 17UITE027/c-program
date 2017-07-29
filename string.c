50c6cf7f58729a81eef0f534334b7804da19fcf2
Dinesh272 <30414317+Dinesh272@users.noreply.github.com>
2017-07-29 21:12:38 +0530

    Update string.c

--- string.c
+++ string.c
@@ -5,6 +5,6 @@ void main()
 string str1,str2;
 printf("Enter the string to be reverse");
 scanf("%s",str1);
-srt2=strrev(str1);
+str2=strrev(str1);
 printf("The reverse of the string is %s",str2);
 }
