92eb3214b40423c6296d7754e0dc4258525e2277
Dinesh272 <30414317+Dinesh272@users.noreply.github.com>
2017-07-29 20:37:53 +0530

    vowels.c
    c program

---
+++ vowels.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int ch;
+printf("Enter the letter is to be check vowel or not?");
+scanf("%d",&ch);
+if(ch==a && ch==i && ch==o && ch==e && ch==u)
+{
+printf("The letter is vowel");
+}
+else
+{
+printf("The letter is consonant");
+}
+getch();
+}
