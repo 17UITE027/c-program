8b0f0f6a706388e4de01727e2504ffef12873222
Dinesh272 <30414317+Dinesh272@users.noreply.github.com>
2017-08-04 20:32:33 +0530

    Create natural.c

---
+++ natural.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int a,i,s=0;
+printf("Enter the value of A");
+scanf("%d",&a);
+for(i=1;i<=a;i++)
+{
+s=s+i;
+}
+printf("The number of natural number is %d",s);
+}
