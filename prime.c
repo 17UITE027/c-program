eaf49815ed2625a305d9e5f1370736b27177e1dc
Dinesh272 <30414317+Dinesh272@users.noreply.github.com>
2017-08-08 16:24:14 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -4,13 +4,13 @@ void main()
 int fl=0,i,n;
 printf("Enter the non_negative number");
 scanf("%d",&n);
-for(i=2;i=n/2;i++)
+for(i=2;i<=n/2;i++)
 {
 if(n%i==0)
 {
 fl=1;
 break;
-}
+}}
 if(fl==0)
 {
 printf("The given number is prime");
