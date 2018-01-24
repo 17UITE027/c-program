f9a7297500e840d584b80e175bf1de111a6414e7
Dinesh272 <30414317+Dinesh272@users.noreply.github.com>
2018-01-24 15:43:24 +0530

    Update edit.c

--- edit.c
+++ edit.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-int a[100],i,n,temp[100];
+int a[100],i,n,temp[100],j;
 printf("Enter the total number of elements \n");
 scanf("%d",&n);
 printf("Enter the elements \n");
@@ -9,21 +9,22 @@ for(i=0;i<n;i++)
 {
 scanf("%d",&a[i]);
 }
-for(i=0;i<n;i++)
+for(i=0;i<n;++i)
 {
-	if(a[i] > a[i+1])
+	for(j=i+1;j<n;++j)
+{
+	if(a[i] > a[j])
 	{
-	void swap()
-	{
+
 		temp[i]=a[i];
-		a[i]=a[i+1];
-		a[i+1]=temp[i];
+		a[i]=a[j];
+		a[j]=temp[i];
 	}
 	}
 }
 
 for(i=0;i<n;i++)
 {
-	printf("%d",temp[i]);
+	printf("%d",a[i]);
 }
 }
