be6b39354169ea2c8c8df43b849950c6fcb2d285
Dinesh272 <30414317+Dinesh272@users.noreply.github.com>
2018-02-02 15:55:49 +0530

    Create wstrcat.c

---
+++ wstrcat.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char s1,s2,s3;
+printf("\n Enter the 1st string \n");
+scanf("%s",s1);
+printf("\n Enter the 2nd string \n");
+scanf("%s",s2);
+s3=(s1+s2);
+printf("The concadination of string is %s",s3);
+}
